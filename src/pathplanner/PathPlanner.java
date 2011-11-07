c2a4ae0403705a63a83b71c41f3bead27fd1e126
Juuso Lehtinen <jupelehtinen@gmail.com>
2011-11-07 18:54:53 +0200

    Parameter for number of iterations

--- src/pathplanner/PathPlanner.java
+++ src/pathplanner/PathPlanner.java
@@ -21,20 +21,28 @@ public class PathPlanner {
         // TODO code application logic here
         ArrayList<Kaupunki> kaupungit;
         
-        if(args.length > 0)
-            kaupungit = reader.readFile(args[0]);
-        else
-            kaupungit = reader.readFile("map.100");
+        int iteraatiot;
         
+        if(args.length > 1){
+            kaupungit = reader.readFile(args[0]);
+            iteraatiot = Integer.parseInt(args[1]);
+        }
+        else {
+            kaupungit = reader.readFile("map.100");
+            iteraatiot = 1000;
+        }
         
         Tulos paras = new Tulos(Integer.MAX_VALUE, new LinkedList<Kaupunki>());
         Tulos tulos;
-        for(int i = 0; i < 1000; i++){
+        for(int i = 0; i < iteraatiot; i++){
+            if(i % 100 == 0)
+                System.out.println("Lasketaan " + i);
+            
             tulos = calculator.calculateRoute(kaupungit);
             if(tulos.pituus < paras.pituus)
                 paras = tulos;
         }
-        System.out.println(paras.pituus);
+        System.out.println("Paras pituus: " + paras.pituus);
         for(Kaupunki k : paras.reitti)
             System.out.println(k.nimi);
         
